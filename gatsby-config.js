52e570ed62064db7740e5f77e543b387da324a41
X <38390595+gadossey@users.noreply.github.com>
2024-01-31 15:36:59 +0000

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: `Improved Biomass Cooking in Africa`,
+    author: `MITWORK GHANA LIMITED`,
+    about: `MAN AND MAN ENTERPRISE targets includes homes, restaurants, peri-urban and rural areas where charcoal and fire wood is predominantly the source of fuel.`,
+    description: `MAN AND MAN ENTERPRISE targets includes homes, restaurants, peri-urban and rural areas where charcoal and fire wood is predominantly the source of fuel.`,
+    siteUrl: `https://manandmanenterprise.com`,
   },
   plugins: [
     {
